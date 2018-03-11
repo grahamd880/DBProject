23b126da6e4b43f44e64f59e86d0df4264a92f3c
David Graham <grahamd880@gmail.com>
2018-03-10 19:39:22 -0700

    admin-1

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,10 +1,67 @@
 var express = require('express');
 var router = express.Router();
-var table = ['employee','customer','item','reviews']
+var crypto = require('crypto');
+var shasum = crypto.createHash('sha512');
+var sequelize = require('sequelize');
+var models = require('../modles');
+var tables = ['Persons','Customers','Employees','Items','Reviews']
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', tables: table });
+  res.render('index', { title:'Admin' });
+});
+
+router.post('/login', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  //shasum.update(password,'utf8');
+  //shasum.digest('utf8');
+  //console.log(shasum);
+
+  res.render('admin', { title: 'Database Administration', tables: tables});
+});
+
+router.get('/getTable/:table', function(req,res,next){
+  var table = req.params.table;
+  switch(table){
+    case 'Persons':
+    models.person.findAll().then(persons =>{
+      res.send(person);
+      persons.array.forEach(person => {
+        console.log(person.get('username'));
+      });
+    });
+    case 'Customers':
+    models.customer.findAll().then(customers =>{
+      res.send(customers);
+    });
+    case 'Employees':
+    models.employee.findAll().then(employees =>{
+      res.send(employees);
+    });
+    case 'Items':
+    models.item.findAll().then(items =>{
+      res.send(items);
+    });
+    case 'Reviews':
+    models.reviews.findAll().then(reviewss =>{
+      res.send(reviewss);
+    });
+
+    break;
+    default:
+    break;
+  }
+  res.send();
+});
+
+router.get('/fillTables', function(req, res, next) {
+  
+  res.render('admin', { title: 'Database Administration', tables: tables});
+});
+
+router.get('/clearTables', function(req, res, next) {
+  res.render('admin', { title: 'Database Administration', tables: tables});
 });
 
 module.exports = router;
